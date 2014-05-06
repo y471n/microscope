f5ebdc52fb220acde872188587110e2891e2dae9
yatin <yatin@yMac.local>
2014-05-06 21:32:11 +0530

    posts list from db now

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,21 +1,5 @@
-var postsData = [
-	{
-		title: 'Introducing Telescope',
-		author: 'Sacha Grief',
-		url : 'http://google.com'
-	},
-	{
-		title: 'Meteor',
-		author: 'Brad Pitt',
-		url: 'http://meteor.com'
-	},
-	{
-		title: 'The Meteor Book',
-		author: 'Angenlina Jolie',
-		url: 'http://facebook.com'
-	}
-];
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });

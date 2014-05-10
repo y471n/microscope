6db7819b9e7e26ce50de129ea5e48c35d4040377
yatin <yatin@yMac.local>
2014-05-10 07:56:45 +0530

    Demonstrate the order that posts appear using a sleep

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -11,8 +11,7 @@ Template.postSubmit.events({
     Meteor.call('post', post, function(error, id) {
 	if (error)
 		return alert(error.reason);
-    	
-	Router.go('postPage', {_id: id});
   	});
+	Router.go('postsList');
   }
 });
